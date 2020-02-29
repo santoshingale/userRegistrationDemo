394aa6e6cdbfe1d058a60615c835657775b3c894
santoshingale <santoshingale876@gmail.com>
2020-02-29 17:17:25 +0530

    second commit

--- src/main/java/com/bridgelabz/demo/service/UserService.java
+++ src/main/java/com/bridgelabz/demo/service/UserService.java
@@ -5,10 +5,13 @@ import com.bridgelabz.demo.dto.UserDTO;
 import com.bridgelabz.demo.exception.UserException;
 import com.bridgelabz.demo.model.User;
 import com.bridgelabz.demo.repository.UserRepo;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,8 +42,12 @@ public class UserService implements IUserService {
     @Override
     public String logInUser(LoginDTO loginDTO) {
         Optional<User> byEmail = userRepo.findByEmail(loginDTO.email);
-        if(byEmail.isPresent()&& passwordEncoder.matches(loginDTO.password,byEmail.get().getPassword())) {
-            return "successfully Loged in";
+        if (byEmail.isPresent() && passwordEncoder.matches(loginDTO.password, byEmail.get().getPassword())) {
+            return /*JWT.create()
+                    .withClaim("email", loginDTO.email)
+                    .sign(Algorithm.HMAC256(System.getenv("secret")));*/
+                    Jwts.builder().claim("email",loginDTO.email).setIssuedAt(new Date(System.currentTimeMillis()))
+                            .setExpiration(new Date(System.currentTimeMillis() + 1000*20)).signWith(SignatureAlgorithm.HS256,System.getenv("secret")).compact();
         }
         throw new UserException("Incorrect email or password");
     }
